f3dfa0ff7f419078efe91c6fab875291d200277f
Jatin Verma <jatin@Jatins-MacBook-Air.local>
2025-10-25 10:59:16 +0530

    First

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of integers to sum:");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter integer " + i + ":");
+            int temorary = sc.nextInt();
+            sum += temorary;
+        }
+        sc.close();
+        System.out.println(sum);
+    }
+}
