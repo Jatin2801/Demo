51653fd0cad78c7369ab6c8a194af55b818e284c
Jatin Verma <jatin@Jatins-MacBook-Air.local>
2025-10-25 11:14:40 +0530

    on_vs

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of integers to sum:");
         int n = sc.nextInt();
-        int sum = 0 // here change ;
+        int sum = 0 ;// here change 
         for (int i = 1; i <= n; i++) {
             System.out.println("Enter integer " + i + ":");
             int temorary = sc.nextInt();
